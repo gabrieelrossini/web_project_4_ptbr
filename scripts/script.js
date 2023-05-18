eb59517a375aafc1b8fa0e3c2c04aa879798a70c
gabrieelrossini <gabrieelrossini23@hotmail.com>
2023-05-18 03:52:22 -0300

    atualizações para o botão de like no projeto

--- scripts/script.js
+++ scripts/script.js
@@ -1,20 +1,28 @@
+//botão de edição
+
 const editButton = document.querySelector(".button__edit");
+
+editButton.addEventListener("click", function() {
+    form.style.display = "flex";
+});
+
+//botão para fechar o formulário
+
 const closeButton = document.querySelector(".button__close");
+
+closeButton.addEventListener("click", function() {
+    event.preventDefault();
+    form.style.display = "none";
+});
+
+//botão para salvar, alterando as informações do formulário
+
 const saveButton = document.querySelector(".button__save");
 const form = document.querySelector(".form");
 const nameInput = document.querySelector(".form__name");
 const infoInput = document.querySelector(".form__info");
 const nameData = document.querySelector(".data__name");
 const infoData = document.querySelector(".data__info");
-
-editButton.addEventListener("click", function() {
-    form.style.display = "flex";
-});
-
-closeButton.addEventListener("click", function() {
-    event.preventDefault();
-    form.style.display = "none";
-});
 
 saveButton.addEventListener("click", function() {
     event.preventDefault();
@@ -23,4 +31,17 @@ saveButton.addEventListener("click", function() {
     nameData.textContent = newName;
     infoData.textContent = newInfo;
     form.style.display = "none";
+});
+
+//botão de like
+
+const heartButton = document.querySelector(".button__heart");
+const heartBlack = document.querySelector("#heart__black");
+
+heartButton.addEventListener("click", function() {
+    if (heartBlack.getAttribute("src") === "./images/heart.svg") {
+        heartBlack.setAttribute("src", "./images/blackheart.svg");
+    } else {
+        heartBlack.setAttribute("src", "./images/heart.svg");
+    }
 });
