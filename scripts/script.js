9acb0aa275950ea1c7d387aa1ee307144a7dc745
gabrieelrossini <gabrieelrossini23@hotmail.com>
2023-05-22 15:35:20 -0300

    atualizações no botão edit e close, faltando botão like

--- scripts/script.js
+++ scripts/script.js
@@ -1,28 +1,23 @@
-//botão de edição
+//seletores
 
 const editButton = document.querySelector(".button__edit");
-
-editButton.addEventListener("click", function() {
-    form.style.display = "flex";
-});
-
-//botão para fechar o formulário
-
-const closeButton = document.querySelector(".button__close");
-
-closeButton.addEventListener("click", function() {
-    event.preventDefault();
-    form.style.display = "none";
-});
-
-//botão para salvar, alterando as informações do formulário
-
 const saveButton = document.querySelector(".button__save");
+const closeButton = document.querySelector(".button__close");
 const form = document.querySelector(".form");
 const nameInput = document.querySelector(".form__name");
 const infoInput = document.querySelector(".form__info");
 const nameData = document.querySelector(".data__name");
 const infoData = document.querySelector(".data__info");
+const formActive = document.querySelector(".form__active");
+
+//botão edit ativo
+
+editButton.addEventListener("click", function() {
+    form.classList.toggle("form-active");
+    form.classList.toggle("form");
+});
+
+//botão save ativo, alterando as informações do formulário
 
 saveButton.addEventListener("click", function() {
     event.preventDefault();
@@ -30,18 +25,16 @@ saveButton.addEventListener("click", function() {
     const newInfo = infoInput.value;
     nameData.textContent = newName;
     infoData.textContent = newInfo;
-    form.style.display = "none";
+    form.classList.toggle("form-active");
+    form.classList.toggle("form");
+});
+
+//botão close ativo
+
+closeButton.addEventListener("click", function() {
+    event.preventDefault();
+    form.classList.toggle("form-active");
+    form.classList.toggle("form");
 });
 
 //botão de like
-
-const heartButton = document.querySelector(".button__heart");
-const heartBlack = document.querySelectorAll(".heart__black");
-
-heartButton.addEventListener("click", function() {
-    if (heartBlack.getAttribute("src") === "./images/heart.svg") {
-        heartBlack.setAttribute("src", "./images/blackheart.svg");
-    } else {
-        heartBlack.setAttribute("src", "./images/heart.svg");
-    }
-});
