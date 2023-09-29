4c57bc25c2a44012e57e8570f711609b735621e6
gabrieelrossini <gabrieelrossini23@hotmail.com>
2023-09-29 16:47:28 -0300

    atualização no clique das imagens, além de efeito nos botões dos forms

--- scripts/script.js
+++ scripts/script.js
@@ -1,15 +1,17 @@
-//botão edit ativo
+//seletores botão edit
 
 const editButton = document.querySelector(".button__edit");
 const form = document.querySelector(".form");
 const formActive = document.querySelector(".form-active");
 
-editButton.addEventListener("click", function() {
-form.classList.toggle("form-active");
-form.classList.toggle("form");
+//efeito de click botão edit
+
+editButton.addEventListener("click", function () {
+  form.classList.toggle("form-active");
+  form.classList.toggle("form");
 });
 
-//botão save ativo, alterando as informações do formulário
+//seletores botão save
 
 const saveButton = document.querySelector(".button__save");
 const nameForm = document.querySelector(".form__name");
@@ -17,24 +19,44 @@ const infoForm = document.querySelector(".form__info");
 const nameData = document.querySelector(".data__name");
 const infoData = document.querySelector(".data__info");
 
-saveButton.addEventListener("click", function() {
-event.preventDefault();
-const newName = nameForm.value;
-const newInfo = infoForm.value;
-nameData.textContent = newName;
-infoData.textContent = newInfo;
-form.classList.toggle("form-active");
-form.classList.toggle("form");
+//função de validação botão save
+
+function validateFields() {
+  const isNameValid = nameForm.checkValidity();
+  const isInfoValid = infoForm.checkValidity();
+
+  saveButton.classList.toggle(
+    "button__save-off",
+    !(isNameValid && isInfoValid)
+  );
+}
+
+nameForm.addEventListener("input", validateFields);
+infoForm.addEventListener("input", validateFields);
+
+//efeito de click botão save
+
+saveButton.addEventListener("click", function (event) {
+  event.preventDefault();
+
+  if (nameForm.checkValidity() && infoForm.checkValidity()) {
+    const newName = nameForm.value;
+    const newInfo = infoForm.value;
+    nameData.textContent = newName;
+    infoData.textContent = newInfo;
+    form.classList.toggle("form-active");
+    form.classList.toggle("form");
+  }
 });
 
 //botão close ativo
 
 const closeButton = document.querySelector(".button__close");
 
-closeButton.addEventListener("click", function() {
-event.preventDefault();
-form.classList.toggle("form-active");
-form.classList.toggle("form");
+closeButton.addEventListener("click", function () {
+  event.preventDefault();
+  form.classList.toggle("form-active");
+  form.classList.toggle("form");
 });
 
 //template cards
@@ -42,171 +64,189 @@ form.classList.toggle("form");
 const cardTemplate = document.querySelector("#card-template");
 const cardContainer = document.querySelector(".cards");
 const cardsData = [
-{
-src:"./images/yosemite.jpg",
-alt:"retrato fotográfico do Vale de Yosemite",
-title: "Vale de Yosemite"
+  {
+    src: "./images/yosemite.jpg",
+    alt: "Vale de Yosemite",
+    title: "Vale de Yosemite",
+  },
 
-},
+  {
+    src: "./images/louise.jpg",
+    alt: "Lago Louise",
+    title: "Lago Louise",
+  },
 
-{
-src:"./images/louise.jpg",
-alt:"retrato fotográfico do Lago Louise",
-title: "Lago Louise"
-},
+  {
+    src: "./images/montanhas.jpg",
+    alt: "Montanhas Carecas",
+    title: "Montanhas Carecas",
+  },
 
-{
-src:"./images/montanhas.jpg",
-alt:"retrato fotográfico das Montanhas Carecas",
-title: "Montanhas Carecas"
-},
+  {
+    src: "./images/latemar.jpg",
+    alt: "Latemar",
+    title: "Latemar",
+  },
 
-{
-src:"./images/latemar.jpg",
-alt:"retrato fotográfico de Latemar",
-title: "Latemar"
-},
+  {
+    src: "./images/vanoise.jpg",
+    alt: "Parque Nacional de Vanoise",
+    title: "Vanoise National Park",
+  },
 
-{
-src:"./images/vanoise.jpg",
-alt:"retrato fotográfico do Parque Nacional de Vanoise",
-title: "Vanoise National Park"
-},
-
-{
-src:"./images/braies.jpg",
-alt:"retrato fotográfico do Lago de Braies",
-title: "Lago di Braies"
-},
+  {
+    src: "./images/braies.jpg",
+    alt: "Lago di Braies",
+    title: "Lago di Braies",
+  },
 ];
 
 function addHeartEvent(card) {
-const buttonHeart = card.querySelector(".button__heart");
+  const buttonHeart = card.querySelector(".button__heart");
 
-buttonHeart.addEventListener("click", () => {
-buttonHeart.classList.toggle("button__heart-active");
-});
+  buttonHeart.addEventListener("click", () => {
+    buttonHeart.classList.toggle("button__heart-active");
+  });
 }
 
 // conteúdo dos cards
 
-cardsData.forEach(data => {
+cardsData.forEach((data) => {
+  const card = cardTemplate.content.cloneNode(true);
 
-const card = cardTemplate.content.cloneNode(true);
-    
-const image = card.querySelector(".card__image");
-image.src = data.src;
-image.alt = data.alt;
-    
-image.addEventListener('click', handleImageClick);
-    
-const cardTitle = card.querySelector(".card__title");
-cardTitle.textContent = data.title;
-    
-const buttonTrash = document.createElement("button");
-buttonTrash.classList.add("button__trash");
-card.querySelector(".card").appendChild(buttonTrash);
-    
-buttonTrash.addEventListener("click", () => {
-buttonTrash.closest(".card").remove();
-});
-    
-addHeartEvent(card);
-    
-cardContainer.appendChild(card);
-    
-});
-    
-// Função de clique na imagem
-function handleImageClick(e) {
-    
-const image = e.currentTarget;
-    
-const src = image.src;
-const imageTitle = image.title;
-    
-openPopup(src, imageTitle);
-    
-}
-    
-// Função para abrir popup
-function openPopup(src, imageTitle){
-    
-const popup = document.querySelector('.popup');
-    
-const closeBtn = document.querySelector('.button__popup');
-    
-closeBtn.addEventListener('click', () => {
-popup.classList.remove('popup-active');
-});
-    
-const popupImage = popup.querySelector('.popup__image');
-const popupTitle = popup.querySelector('.popup__title');
-    
-popupImage.src = src;
-popupTitle.textContent = imageTitle;
-    
-popup.classList.add('popup-active');
-    
-}
-  
+  const image = card.querySelector(".card__image");
+  image.src = data.src;
+  image.alt = data.alt;
 
-//botão add ativo
+  image.addEventListener("click", handleImageClick);
+
+  const cardTitle = card.querySelector(".card__title");
+  cardTitle.textContent = data.title;
+
+  const buttonTrash = document.createElement("button");
+  buttonTrash.classList.add("button__trash");
+  card.querySelector(".card").appendChild(buttonTrash);
+
+  buttonTrash.addEventListener("click", () => {
+    buttonTrash.closest(".card").remove();
+  });
+
+  addHeartEvent(card);
+
+  cardContainer.appendChild(card);
+});
+
+//seletores botão add
 
 const addButton = document.querySelector(".button__add");
 const create = document.querySelector(".create");
 
-addButton.addEventListener("click", function() {
-create.classList.toggle("create-active");
-create.classList.toggle("create");
+//efeito de click botão add
+
+addButton.addEventListener("click", function () {
+  create.classList.toggle("create-active");
+  create.classList.toggle("create");
 });
 
-//botão create ativo
-
+// Seletores dos botões "Create"
 const createButton = document.querySelector(".button__create");
 const nameCreate = document.querySelector(".create__name");
 const infoCreate = document.querySelector(".create__info");
 
-createButton.addEventListener("click", function(event) {
+// Função de validação dos campos
+function validateCreateFields() {
+  const isNameValid = nameCreate.checkValidity();
+  const isInfoValid = infoCreate.checkValidity();
 
-event.preventDefault();
-const name = nameCreate.value;
-const imageSrc = infoCreate.value;
-const card = cardTemplate.content.cloneNode(true);
+  createButton.classList.toggle(
+    "button__create-off",
+    !(isNameValid && isInfoValid)
+  );
+}
 
-const title = card.querySelector(".card__title");
-title.textContent = name;
+// Evento de input nos campos "Título" e "URL da imagem"
+nameCreate.addEventListener("input", validateCreateFields);
+infoCreate.addEventListener("input", validateCreateFields);
 
-const image = card.querySelector(".card__image");
-image.src = imageSrc;
-image.alt = name;
+// Evento de clique do botão "Create"
+createButton.addEventListener("click", function (event) {
+  event.preventDefault();
 
-const buttonTrash = document.createElement("button");
-buttonTrash.classList.add("button__trash");
-card.querySelector(".card").appendChild(buttonTrash);
+  // Verifica se os campos são válidos
+  if (!nameCreate.checkValidity() || !infoCreate.checkValidity()) {
+    return; // Sai precocemente se os campos não forem válidos
+  }
 
-buttonTrash.addEventListener("click", () => {
-buttonTrash.closest(".card").remove();
+  // Criação do card
+  const name = nameCreate.value;
+  const imageSrc = infoCreate.value;
+  const card = cardTemplate.content.cloneNode(true);
+
+  const title = card.querySelector(".card__title");
+  title.textContent = name;
+
+  const image = card.querySelector(".card__image");
+  image.src = imageSrc;
+  image.alt = name;
+
+  const buttonTrash = document.createElement("button");
+  buttonTrash.classList.add("button__trash");
+  card.querySelector(".card").appendChild(buttonTrash);
+
+  buttonTrash.addEventListener("click", () => {
+    buttonTrash.closest(".card").remove();
+  });
+
+  addHeartEvent(card); // Adiciona o evento de clique no coração
+
+  addClickEventToImage(card); // Adiciona o evento de clique na imagem
+
+  cardContainer.appendChild(card);
+
+  nameCreate.value = "";
+  infoCreate.value = "";
+
+  create.classList.toggle("create-active");
+  create.classList.toggle("create");
 });
 
-addHeartEvent(card);
+// Função de clique na imagem
+function addClickEventToImage(card) {
+  const image = card.querySelector(".card__image");
+  image.addEventListener("click", handleImageClick);
+}
 
-cardContainer.appendChild(card);
+function handleImageClick(e) {
+  const image = e.currentTarget;
+  const src = image.src;
+  const imageTitle = image.alt;
+  openPopup(src, imageTitle);
+}
 
-nameCreate.value = "";
-infoCreate.value = "";
+// Função para abrir popup
+function openPopup(src, imageTitle) {
+  const popup = document.querySelector(".popup");
+  const closeBtn = document.querySelector(".button__popup");
 
-create.classList.toggle("create-active");
-create.classList.toggle("create");
+  closeBtn.addEventListener("click", () => {
+    popup.classList.remove("popup-active");
+  });
 
-});
+  const popupImage = popup.querySelector(".popup__image");
+  const popupTitle = popup.querySelector(".popup__title");
+
+  popupImage.src = src;
+  popupTitle.textContent = imageTitle;
+
+  popup.classList.add("popup-active");
+}
 
 //botão exit ativo
 
 const exitButton = document.querySelector(".button__exit");
 
-exitButton.addEventListener("click", function() {
-event.preventDefault();
-create.classList.toggle("create-active");
-create.classList.toggle("create");
+exitButton.addEventListener("click", function () {
+  event.preventDefault();
+  create.classList.toggle("create-active");
+  create.classList.toggle("create");
 });
